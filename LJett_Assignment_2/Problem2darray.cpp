37a046d3c4c2d91e1cb4ee623152582a38d27eeb
loganmjett <ljett@chapman.edu>
2020-01-30 06:31:29 +0000

    Updated

---
+++ LJett_Assignment_2/Problem2darray.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc char **argv){
+  int a[4][5];
+  int i;
+  int j;
+
+  for (i = 0; i < 4; ++i) {
+    for (j = 0; j < 5; ++j) {
+      cout << "Enter a number: ";
+      cin >> a[i][j];
+
+    }
+  }
+  for (i = 0; i < 4; ++i) {
+    for (j = 0; j < 5; ++j) {
+      cout << a[i][j];
+
+    }
+    cout << endl;
+  }
+return 0;
+}
